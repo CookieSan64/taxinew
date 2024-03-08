d9f3a84eff069a11625631c398a7fae1dced4c79
ChouCookieSan <choucookiesan@gmail.com>
2024-03-08 02:32:12 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+resource_manifest_version "77731fab-63ca-442c-a67b-abc70f28dfa5"
+
+data_file "INTERIOR_PROXY_ORDER_FILE" "interiorproxies.meta"
+
+--files { "stream/interiorproxies.meta" }
+
+author "ChouCookieSan"
+description "Taxi"
+version "1.0.0"
+
+fx_version "cerulean"
+game "gta5"
+
+this_is_a_map 'yes'
